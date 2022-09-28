b9d2907dc865d957e4a8811d2a240acfa0ae36d7
SRoss <srossouw@uwm.edu>
2022-09-28 15:57:33 -0500

    added starter code and pizza models/routes

---
+++ models/Pizza.js
@@ -0,0 +1,31 @@
+const {Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+    pizzaName: {
+      type: String
+    },
+    createdBy: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    size: {
+      type: String,
+      default: 'Large'
+    },
+    toppings: [],
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment' 
+        }
+      ]
+  });
+
+  // create the Pizza model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+// export the Pizza model
+module.exports = Pizza;
